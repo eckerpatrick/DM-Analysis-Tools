595e979871e9211d98ed559219779bb97c314e8a
Patrick Ecker <patrick.ecker@desy.de>
2022-08-23 15:06:11 +0200

    setting up the repo

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="DM-Analysis-Tools",
+    version="0.1.0",
+    packages=find_packages(),
+    url="https://github.com/eckerpatrick/DM-Analysis-Tools",
+    license="",
+    author="Patrick Ecker",
+    author_email="patrick.ecker@kit.edu",
+    description="Analysis Tools for a Dark Matter Physics Analysis.",
+)
